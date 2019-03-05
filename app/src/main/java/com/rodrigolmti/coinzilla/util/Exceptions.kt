5beb91a485ea52cc4555bf46b5699f1284ceaa1d
Rodrigo Lopes <rodrigolmti@gmail.com>
2019-03-04 23:03:53 -0300

    Missing commit files

---
+++ app/src/main/java/com/rodrigolmti/coinzilla/util/Exceptions.kt
@@ -0,0 +1,6 @@
+package com.rodrigolmti.coinzilla.util
+
+class ApiException(override val message: String) : Exception()
+class ConnectionException : Exception()
+class RtfmException(message: String) : NoSuchMethodException(message)
+class TokenValid : Exception()
